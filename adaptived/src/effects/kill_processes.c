34d6eceb6f343bfc8ee090f19264026a8152f353
Tom Hromatka <tom.hromatka@oracle.com>
2024-11-01 14:34:24 -0600

    effects: Fix coverity warning in kill_processes_init()
    Fix the following coverity warning by initializing opts->proc_name to
    '\0' instead of '0'.
    
    	512051 Memset fill value of '0'
    	The buffer will be filled with ASCII character '0' instead of
    	actual zero bytes.
    
    	In _kill_processes_init: A memset fill value of ASCII character
    	'0' is likely intended to be 0 (CWE-665)
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
    (cherry picked from commit d38cbdd78ee81877710ea8722d35b8c0b67ab2d9)

--- adaptived/src/effects/kill_processes.c
+++ adaptived/src/effects/kill_processes.c
@@ -102,7 +102,7 @@ static int _kill_processes_init(struct adaptived_effect * const eff, struct json
 		goto error;
 	}
 
-	memset(opts->proc_names, '0', sizeof(char *) * opts->proc_name_cnt);
+	memset(opts->proc_names, '\0', sizeof(char *) * opts->proc_name_cnt);
 
 	for (i = 0; i < opts->proc_name_cnt; i++) {
 		proc_name_obj = json_object_array_get_idx(proc_names_obj, i);
